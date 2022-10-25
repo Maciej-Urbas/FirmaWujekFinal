5d14097f602e5a915cc0b845556e4fe526367dfd
Maciej Urbas <maciej.urbas3@gmail.com>
2022-10-25 12:14:23 +0200

    :sparkles: Final response & button `toggle()` func

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isExpanded = false;
 
-  ngOnInit(): void {
+  collapse() {
+    this.isExpanded = false;
   }
 
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  ngOnInit(): void {}
+
+  zjedzNaDol() {
+    if (window.innerWidth < 746) {
+      this.toggle();
+      const butt = document.getElementsByClassName('footer-down-container_p');
+      let ss = butt[0].getBoundingClientRect().top;
+
+      setTimeout(() => {
+        window.scroll({
+          top: ss,
+          left: 0,
+          behavior: 'smooth',
+        });
+      }, 400);
+    } else {
+      const butt = document.getElementsByClassName('footer-down-container_p');
+      let ss = butt[0].getBoundingClientRect().top;
+      ss = ss + 200;
+
+      window.scroll({
+        top: ss,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  }
 }
